fda7a51951ce2a911e212a6cf510a6ea0c04eaa0
paras-saxena11 <77022754+paras-saxena11@users.noreply.github.com>
2022-07-16 05:06:34 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,12 @@
+// NAVBAR
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const navlinks = document.querySelector('.navlinks');
+  
+    burger.addEventListener('click' , () => {
+        navlinks.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+  }
+  
+  navslide();
---
+++ homepage.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// NAVBAR
+const navslide = () => {
+  const burger = document.querySelector('.burger');
+  const navlinks = document.querySelector('.navlinks');
+
+  burger.addEventListener('click' , () => {
+      navlinks.classList.toggle('nav-active');
+      burger.classList.toggle('toggle');
+  });
+}
+
+navslide();
